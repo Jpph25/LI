9930f953b793930b8031998de5942fbb5329ca0b
João Gomes <jppgomes06@gmail.com>
2024-10-01 01:20:50 +0100

    Resolution of exercises

--- Fichas/ficha2.hs
+++ Fichas/ficha2.hs
@@ -71,9 +71,64 @@ moveVeloxy (x,y) (vx,vy) t = (x + vx * t, y + vy * t)
 -- 8
 
 data Figura =
-             |Circulo Ponto Double 
-             |Rectangulo Ponto Ponto 
-             |Quadrado Ponto Double 
+             Circulo Ponto Double   |
+             Rectangulo Ponto Ponto | 
+             Quadrado Ponto Double 
              deriving (Show,Eq)
 
 --  a) 
+inFig :: Figura -> Ponto -> String 
+inFig (Circulo (xc,yc) r) (x1,y1) = if   sqrt ((xc - x1)^2 + (yc - y1)^2) > r
+                                    then "O ponto não pertence a figura"
+                                    else "O ponto pertence a figura" 
+inFig (Rectangulo (xr1,yr1) (xr2,yr2)) (x2,y2)
+                                              | x2 > min xr1 xr2 || x2 < max xr1 xr2 
+                                               = "O ponto não pertence a figura"  
+                                              | y2 > min yr1 yr2 || y2 < max yr1 yr2 
+                                               = "O ponto não pertence a figura"    
+                                              | otherwise = "O ponto pertence a figura"
+inFig (Quadrado (xq,yq) l) (x3,y3) 
+                                  | x3 > (xq + l) || x3 < xq  
+                                   = "O ponto não pertence a figura"  
+                                  | y3 > (yq + l) || y3 < yq  
+                                   = "O ponto não pertence a figura"    
+                                  | otherwise = "O ponto pertence a figura" 
+
+--  b) 
+menorQuadrado :: Figura -> Figura 
+menorQuadrado (Circulo (xc,yc) r) 
+               = (Quadrado (xc- r, yc - r) (2 * r)) 
+menorQuadrado (Rectangulo (xr1,yr1) (xr2,yr2)) 
+               = (Quadrado (min xr1 xr2, max yr1 yr2) 
+                 (max (abs (xr1 - xr2)) (abs (yr1-yr2)))) 
+menorQuadrado (Quadrado (xq,yq) l)  
+               = (Quadrado (xq,yq) l)  
+
+--  c)
+maiorCirculo :: Figura -> Figura
+maiorCirculo (Quadrado (xq,yq) l) 
+              = (Circulo (xq + l/2, yq + l/2) 
+                (sqrt ((xq - xq + l/2 )^2 + (yq - yq + l/2)^2)))  
+maiorCirculo (Rectangulo (xr1,yr1) (xr2,yr2)) 
+              = (Circulo ((xr1 + xr2)/2, (yr1+yr2)/2)
+                (sqrt ((min xr1 xr2) - (xr1 + xr2)/2) +  ((max yr1 yr2) - (yr1 + yr2)/2)))
+maiorCirculo (Circulo (xc,yc) r)  
+              = (Circulo (xc,yc) r) 
+
+-- d)   
+contida :: Figura -> Figura -> Bool
+contida (Circulo (xc1,yc1) r1) (Circulo (xc2,yc2) r2) 
+         = if   (sqrt ((xc1 - xc2)^2 + (yc1 - yc2)^2) + r1) <= r2
+           then True
+           else False  
+contida (Quadrado (xq1,yq1) l1) (Quadrado (xq2,yq2) l2)
+         = if   (sqrt ((xq1 - xq2)^2 + (yq1 - yq2)^2) + l1 * sqrt 2) <= l2 * sqrt 2 
+           then True
+           else False  
+contida (Rectangulo (xr11,yr11) (xr12,yr12)) (Rectangulo (xr21,yr21) (xr22,yr22))
+         = if (    (min xr12 xr11) >= (min xr21 xr22) 
+                 && (min yr12 yr11) >= (min yr21 yr22)
+                && (max xr12 xr11) <= (max xr21 xr22) 
+                 && (max yr12 yr11) <= (max yr21 yr22))   
+           then True
+           else False                           
