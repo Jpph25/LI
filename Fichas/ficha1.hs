528c059cd5f7d1f57311a301e2ef85e0ff7a172f
João Gomes <jppgomes06@gmail.com>
2024-10-06 16:29:42 +0100

    organization of the code

--- Fichas/ficha1.hs
+++ Fichas/ficha1.hs
@@ -1,105 +1,143 @@
--- Ficha 1
+--   // FICHA 1 \\
 
--- 2
---  a)
-lPrimUlt :: [a] -> (a,a)
-lPrimUlt l = (head l,last l)  
+-- 2.
 
---  b)
-nPrimUlt :: [String] -> (String,String)
-nPrimUlt l = (head l,last l)
+--   a)
+-- Gives a pair with  the first and last element of a given list
+lFstLst :: [a] -> (a,a)
+lFstLst l 
+       = (head l,last l)  
 
---  c)
-nPrimUltv :: [String] -> String
-nPrimUltv l = if   length l == 0 
-              then " "
-              else head l ++ " " ++ last l 
+--   b)
+-- Gives a pair with the fist anda last name 
+nFstLst :: [String] -> (String,String)
+nFstLst l 
+        = (head l,last l)
 
---  d)
-tApel :: [String] -> Int
-tApel l = if   length l == 0
-          then 0
-          else length (last l)
+--   c)
+-- Gives back the fist and last name or nothing in case the list is empty  
+nFstLstv :: [String] -> String
+nFstLstv l 
+        = if   length l == 0 
+          then " "
+          else head l ++ " " ++ last l 
 
---  e) 
-pertChar :: Char -> String -> Bool  
-pertChar a l = elem a l         
+--   d)
+-- Calculates the size of the last name  
+sLast :: [String] -> Int
+sLast l 
+     = if   length l == 0
+       then 0
+       else length (last l)
 
---  f) 
-apag :: [a] -> [a] 
-apag l = if   even (length l) 
+--   e)
+-- Verifies if a given charcter belongs to a given word 
+isinChar :: Char -> String -> Bool  
+isinChar a l 
+        = elem a l         
+
+--   f) 
+-- Removes the first elemnet if length is even, and removes the last
+-- if its odd
+remv :: [a] -> [a] 
+remv l = if   even (length l) 
          then tail l
          else init l 
 
---  g)
--- i.
+--   g)
+--  i.
+-- Calculates the list of the two shifts together
 total :: [a] -> [a] -> [a]
-total l a = l ++ a
+total l a 
+     = l ++ a
 
--- ii.
+--  ii.
+-- Calculates the size of the two shifts together 
 nTotal :: [a] -> [a] -> Int
-nTotal l a = length (l ++ a)
+nTotal l a 
+      = length (l ++ a)
 
--- iii.
-turnosDif :: [a] -> [a] -> Int 
-turnosDif a l = abs (length a - length l)
+--  iii.
+-- Calculates the difference in size of the two shifts
+shiftsDif :: [a] -> [a] -> Int 
+shiftsDif a l 
+         = abs (length a - length l)
 
--- iv. 
-pertnTurno :: Eq a => [a] -> a -> String
-pertnTurno l a = if   elem a l == True 
-                 then "Sim"
-                 else "Nao"
+--  iv.
+-- Verifies if a student belongs to a given shift  
+isinShifts :: Eq a => [a] -> a -> String
+isinShifts l a 
+          = if   elem a l == True 
+            then "Yes"
+            else "No"
 
---  h)
-somaOrdl :: [Int] -> [Int] -> [Int]
-somaOrdl a b = if   length a > length b 
-               then b ++ a 
-               else a ++ b   
+--   h)
+-- Sums two lists making the smaller one first 
+sumOrdl :: [Int] -> [Int] -> [Int]
+sumOrdl a b 
+       = if   length a > length b 
+         then b ++ a 
+         else a ++ b   
 
---  i) 
-somaOrdh :: [Int] -> [Int] -> [Int]
-somaOrdh a b = if   head a > head b 
-               then b ++ a
-               else a ++ b
+--   i)
+-- Sums two lists making the one with the smaller head first 
+sumOrdh :: [Int] -> [Int] -> [Int]
+sumOrdh a b 
+       = if   head a > head b 
+         then b ++ a
+         else a ++ b
 
---  j)
-parList :: ([a],[a]) -> (a,[a])
-parList (a,b) = (head a,b) 
+--   j)
+-- Gives a pair with the head of the first list and the same last list
+pairList :: ([a],[a]) -> (a,[a])
+pairList  (a,b) 
+       = (head a,b) 
 
 
---  k)
-nomePlUltn :: [String] -> String 
-nomePlUltn a = [head(head a)] ++ "." ++ last a 
+--   k)
+-- Gives the first letter of the first name and the last name 
+nameFstlst :: [String] -> String 
+nameFstlst a 
+          = [head(head a)] ++ "." ++ last a 
 
---  l) 
+--   l) 
+-- Gives the last element of the head of the list and zero if its empty
 parListInt :: [(Int,Int)] -> Int
-parListInt a = if   length a == 0
-               then 0
-               else snd(head a)   
+parListInt a 
+          = if   length a == 0
+            then 0
+            else snd(head a)   
 
---  m)
-sumParList :: [(Int,Int)] -> Int
-sumParList a = if   length a == 0
-               then 0
-               else fst(head a) + snd (head a)
+--   m)
+-- Sums the fist and last element of the pair of the first element of the list
+-- When the list is empty gives zero 
+sumPairList :: [(Int,Int)] -> Int
+sumPairList a
+          = if   length a == 0
+            then 0
+            else fst(head a) + snd (head a)
 
---  n) 
+--   n)
+-- Gives the name of the younger person
 personYoung :: (String,Int) -> (String,Int) -> String
-personYoung (a,b) (c,d) = if   b > d
-                          then c
-                          else a 
+personYoung (a,b) (c,d) 
+           = if   b > d
+             then c
+             else a 
 
---  o)
+--   o)
+-- Calculates the coordinates of the lower right vertex
 f :: (Int,Int) -> Int -> (Int,Int)
-f (x,y) l = (x + l, y - l)
+f (x,y) l 
+ = (x + l, y - l)
 
--- p) 
+--   p)
+-- Calculates the area of the topmost square 
+-- or the smaller area in case they are at tha same level  
 g :: ((Int, Int), Int) -> ((Int, Int), Int) -> Int 
-g ((x1,y1), l1) ((x2,y2), l2) = if   y1 == y2 
-                                then if   l1 > l2
-                                     then l2^2
-                                     else l1^2
-                                else if   y1 > y2 
-                                     then l1^2
-                                     else l2^2
-                                        
+g ((x1,y1), l1) ((x2,y2), l2) 
+ | y1 == y2 && l1 > l2 = l2^2
+ | y1 == y2 && l1 < l2 = l1^2
+ | y1 >  y2            = l1^2     
+ | y1 <  y2            = l2^2
+
